bef745808efbba29a23be3466517ae4d67b4672e
Prince Narteh <prinarteh@gmail.com>
2022-06-11 23:44:13 +0000

    adds course entity

---
+++ server/src/modules/course/course.entity.ts
@@ -0,0 +1,28 @@
+import { Field, ID, ObjectType } from "type-graphql";
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+@ObjectType()
+export class Course {
+  @PrimaryGeneratedColumn()
+  @Field(() => ID)
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @Column({
+    type: "timestamptz",
+    name: "created_at",
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamptz", name: "updated_at" })
+  updatedAt: Date;
+}
